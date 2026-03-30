b625363ff4c3a625e6dac010ee31943dd8dddbe2
Sena Delase Anyomi <senanyoming@gmail.com>
2026-03-30 21:24:58 +0000

    fixed

--- dispatch-service/src/index.js
+++ dispatch-service/src/index.js
@@ -24,7 +24,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-])
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined', { stream: { write: (msg) => logger.info(msg.trim()) } }));
